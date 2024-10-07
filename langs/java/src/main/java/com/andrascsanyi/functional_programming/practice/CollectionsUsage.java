891747e674523f5f1615dcfef4c739f3a5b74f5c
Andras Csanyi <andras.csanyi@pm.me>
2024-10-07 18:08:11 +0200

    first try

---
+++ langs/java/src/main/java/com/andrascsanyi/functional_programming/practice/CollectionsUsage.java
@@ -0,0 +1,10 @@
+package com.andrascsanyi.functional_programming.practice;
+
+import java.util.List;
+
+public class CollectionsUsage {
+
+    private final List<String> list = List.of("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+     
+
+}
